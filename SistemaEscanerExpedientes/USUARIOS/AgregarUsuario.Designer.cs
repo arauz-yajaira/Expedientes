e0cc04d1244b9dca6d3319e98986252b14d1febe
Yajaira Arauz <67719041+arauz-yajaira@users.noreply.github.com>
2023-08-31 10:13:20 -0600

    Archivos

---
+++ SistemaEscanerExpedientes/USUARIOS/AgregarUsuario.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace SistemaEscanerExpedientes.USUARIOS
+{
+    partial class AgregarUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LbUser = new System.Windows.Forms.Label();
+            this.LbFecha = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BTNCambiar = new System.Windows.Forms.Button();
+            this.BTNModificar = new System.Windows.Forms.Button();
+            this.cbTipoUsuario = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.LBUsuario = new System.Windows.Forms.Label();
+            this.BTNAgregar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtContra = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.DGVUsuarios = new System.Windows.Forms.DataGridView();
+            this.cbEstado = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.BTNLimpiar = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVUsuarios)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LbUser
+            // 
+            this.LbUser.AutoSize = true;
+            this.LbUser.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LbUser.Location = new System.Drawing.Point(12, 9);
+            this.LbUser.Name = "LbUser";
+            this.LbUser.Size = new System.Drawing.Size(56, 18);
+            this.LbUser.TabIndex = 14;
+            this.LbUser.Text = "Usuario";
+            // 
+            // LbFecha
+            // 
+            this.LbFecha.AutoSize = true;
+            this.LbFecha.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LbFecha.Location = new System.Drawing.Point(502, 9);
+            this.LbFecha.Name = "LbFecha";
+            this.LbFecha.Size = new System.Drawing.Size(47, 18);
+            this.LbFecha.TabIndex = 17;
+            this.LbFecha.Text = "Fecha";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Menu;
+            this.panel1.Controls.Add(this.BTNLimpiar);
+            this.panel1.Controls.Add(this.cbEstado);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.BTNCambiar);
+            this.panel1.Controls.Add(this.BTNModificar);
+            this.panel1.Controls.Add(this.cbTipoUsuario);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.LBUsuario);
+            this.panel1.Controls.Add(this.BTNAgregar);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtContra);
+            this.panel1.Controls.Add(this.txtUsuario);
+            this.panel1.Location = new System.Drawing.Point(30, 83);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(395, 374);
+            this.panel1.TabIndex = 18;
+            // 
+            // BTNCambiar
+            // 
+            this.BTNCambiar.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNCambiar.Location = new System.Drawing.Point(204, 272);
+            this.BTNCambiar.Name = "BTNCambiar";
+            this.BTNCambiar.Size = new System.Drawing.Size(97, 40);
+            this.BTNCambiar.TabIndex = 77;
+            this.BTNCambiar.Text = "Cambiar Contraseña";
+            this.BTNCambiar.UseVisualStyleBackColor = true;
+            // 
+            // BTNModificar
+            // 
+            this.BTNModificar.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNModificar.Location = new System.Drawing.Point(106, 272);
+            this.BTNModificar.Name = "BTNModificar";
+            this.BTNModificar.Size = new System.Drawing.Size(92, 40);
+            this.BTNModificar.TabIndex = 76;
+            this.BTNModificar.Text = "Modificar";
+            this.BTNModificar.UseVisualStyleBackColor = true;
+            this.BTNModificar.Click += new System.EventHandler(this.BTNModificar_Click);
+            // 
+            // cbTipoUsuario
+            // 
+            this.cbTipoUsuario.AutoCompleteCustomSource.AddRange(new string[] {
+            "Admin",
+            "User"});
+            this.cbTipoUsuario.FormattingEnabled = true;
+            this.cbTipoUsuario.Location = new System.Drawing.Point(156, 191);
+            this.cbTipoUsuario.Name = "cbTipoUsuario";
+            this.cbTipoUsuario.Size = new System.Drawing.Size(185, 21);
+            this.cbTipoUsuario.TabIndex = 75;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(34, 194);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(110, 18);
+            this.label6.TabIndex = 74;
+            this.label6.Text = "Tipo de Usuario";
+            // 
+            // LBUsuario
+            // 
+            this.LBUsuario.AutoSize = true;
+            this.LBUsuario.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBUsuario.Location = new System.Drawing.Point(101, 32);
+            this.LBUsuario.Name = "LBUsuario";
+            this.LBUsuario.Size = new System.Drawing.Size(175, 29);
+            this.LBUsuario.TabIndex = 72;
+            this.LBUsuario.Text = "Crear Usuario";
+            // 
+            // BTNAgregar
+            // 
+            this.BTNAgregar.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNAgregar.Location = new System.Drawing.Point(16, 272);
+            this.BTNAgregar.Name = "BTNAgregar";
+            this.BTNAgregar.Size = new System.Drawing.Size(83, 40);
+            this.BTNAgregar.TabIndex = 71;
+            this.BTNAgregar.Text = "Agregar";
+            this.BTNAgregar.UseVisualStyleBackColor = true;
+            this.BTNAgregar.Click += new System.EventHandler(this.BTNAgregar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(2, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 18);
+            this.label2.TabIndex = 70;
+            this.label2.Text = "Ingrese Contraseña:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 18);
+            this.label1.TabIndex = 69;
+            this.label1.Text = "Ingrese Usuario:";
+            // 
+            // txtContra
+            // 
+            this.txtContra.Location = new System.Drawing.Point(149, 139);
+            this.txtContra.Multiline = true;
+            this.txtContra.Name = "txtContra";
+            this.txtContra.Size = new System.Drawing.Size(199, 35);
+            this.txtContra.TabIndex = 68;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(149, 98);
+            this.txtUsuario.Multiline = true;
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(199, 35);
+            this.txtUsuario.TabIndex = 67;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.DGVUsuarios);
+            this.panel2.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.panel2.Location = new System.Drawing.Point(464, 83);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(434, 551);
+            this.panel2.TabIndex = 19;
+            // 
+            // DGVUsuarios
+            // 
+            this.DGVUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVUsuarios.Location = new System.Drawing.Point(29, 48);
+            this.DGVUsuarios.Name = "DGVUsuarios";
+            this.DGVUsuarios.Size = new System.Drawing.Size(381, 460);
+            this.DGVUsuarios.TabIndex = 0;
+            // 
+            // cbEstado
+            // 
+            this.cbEstado.AutoCompleteCustomSource.AddRange(new string[] {
+            "Admin",
+            "User"});
+            this.cbEstado.FormattingEnabled = true;
+            this.cbEstado.Location = new System.Drawing.Point(156, 230);
+            this.cbEstado.Name = "cbEstado";
+            this.cbEstado.Size = new System.Drawing.Size(120, 21);
+            this.cbEstado.TabIndex = 79;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(92, 233);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 18);
+            this.label3.TabIndex = 78;
+            this.label3.Text = "Estado";
+            // 
+            // BTNLimpiar
+            // 
+            this.BTNLimpiar.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNLimpiar.Location = new System.Drawing.Point(307, 274);
+            this.BTNLimpiar.Name = "BTNLimpiar";
+            this.BTNLimpiar.Size = new System.Drawing.Size(78, 40);
+            this.BTNLimpiar.TabIndex = 80;
+            this.BTNLimpiar.Text = "Limpiar";
+            this.BTNLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // AgregarUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(921, 707);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.LbFecha);
+            this.Controls.Add(this.LbUser);
+            this.Name = "AgregarUsuario";
+            this.Text = "AgregarUsuario";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DGVUsuarios)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LbUser;
+        private System.Windows.Forms.Label LbFecha;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button BTNCambiar;
+        private System.Windows.Forms.Button BTNModificar;
+        private System.Windows.Forms.ComboBox cbTipoUsuario;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label LBUsuario;
+        private System.Windows.Forms.Button BTNAgregar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtContra;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView DGVUsuarios;
+        private System.Windows.Forms.ComboBox cbEstado;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button BTNLimpiar;
+    }
+}
---
+++ SistemaEscanerExpedientes/USUARIOS/AgregarUsuario.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+
+namespace SistemaEscanerExpedientes.USUARIOS
+{
+    public partial class AgregarUsuario : Form
+    {
+
+        ExpedienteEntities Entity = new ExpedienteEntities();
+    
+        long IdUsuario = 0;
+        bool Modificar = false;
+
+        public AgregarUsuario()
+        {
+            InitializeComponent();
+            CargarDatosDGV();
+        }
+           
+        
+        private void CargarDatosDGV()
+        {
+            var TUsuario = from p in Entity.Usuarios
+                           select new
+                           {
+                               p.IdUsuario,
+                               p.Usuario,
+                               p.FechaIngreso,
+                               p.FechaModificado,
+                               p.EstadoUsuario.EstadoUsuario1,
+                               p.TipoUsuario.TipoUsuario1
+                           };
+            DGVUsuarios.DataSource = TUsuario.CopyAnonymusToDataTable();
+
+            var Tipos = from c in Entity.TipoUsuario
+                        select new
+                        {
+                            c.IdTipoUsuario,
+                            c.TipoUsuario1,
+                        };
+            DataTable DTU = Tipos.CopyAnonymusToDataTable();
+            cbTipoUsuario.DataSource = DTU;
+            cbTipoUsuario.ValueMember = DTU.Columns[0].ColumnName;
+            cbTipoUsuario.DisplayMember = DTU.Columns[1].ColumnName;
+
+            var Estado = from e in Entity.EstadoUsuario
+                        select new
+                        {
+                            e.IdEstado,
+                            e.EstadoUsuario1,
+                        };
+            DataTable E = Estado.CopyAnonymusToDataTable();
+            cbEstado.DataSource = E;
+            cbEstado.ValueMember = E.Columns[0].ColumnName;
+            cbEstado.DisplayMember = E.Columns[1].ColumnName;
+
+
+        }
+
+        public class Hash
+        {
+            public static string obtenerHash256(string text)
+            {
+
+                byte[] bytes = Encoding.Unicode.GetBytes(text);
+                SHA256Managed hashString = new SHA256Managed();
+                byte[] hash = hashString.ComputeHash(bytes);
+                string hashStr = string.Empty;
+                foreach (byte x in hash)
+                {
+                    hashStr += String.Format("{0:x2}", x);
+                }
+                return hashStr;
+            }
+        }
+
+        private void BTNAgregar_Click(object sender, EventArgs e)
+        {
+            //Validacion de llenado
+            if (txtUsuario.Text.Equals("") || txtContra.Text.Equals("") || cbEstado.Equals("") || cbTipoUsuario.Equals(""))
+            {
+                MessageBox.Show("Ingrese Todos los Datos, para agregar", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            String Pass = Hash.obtenerHash256(txtContra.Text);
+            var user = Entity.Usuarios.FirstOrDefault(x => x.Usuario == txtUsuario.Text);//determina si hay existencia de usuario iguales
+            if (Modificar)
+            {
+                var TUsuarios = Entity.Usuarios.FirstOrDefault(x => x.IdUsuario == IdUsuario); //Validacion de existencia de variable IdUsuario
+                TUsuarios.Usuario = txtUsuario.Text;
+                TUsuarios.IdTipoUsuario = cbTipoUsuario.SelectedIndex;
+                TUsuarios.IdEstado = cbEstado.SelectedIndex;
+
+                Entity.SaveChanges();
+            }
+            else
+            {
+                if (user != null)
+                {
+                    MessageBox.Show("Este nombre de Usuario ya Existe", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txtUsuario.Focus();
+                    return;
+                }
+                Usuarios TbUsuarios = new Usuarios
+                {
+
+                    Usuario = txtUsuario.Text,
+                    Contra = txtContra.Text,
+                    FechaIngreso = DateTime.Now,
+                    FechaModificado = DateTime.Now,
+                    IdEstado = cbEstado.SelectedIndex,
+                    IdTipoUsuario = cbTipoUsuario.SelectedIndex
+                };
+                Entity.Usuarios.Add(TbUsuarios);
+                Entity.SaveChanges();
+            }
+
+        }
+
+        private void BTNModificar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
